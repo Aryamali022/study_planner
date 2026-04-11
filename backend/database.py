932d684dae28d2c068e9dd27b95af12ad78224e1
Aryamali022 <arya.malib@gmail.com>
2026-04-11 14:15:19 +0530

    set db name correctly

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
+MONGO_DB = os.getenv("MONGO_DB", "task_manager")
 client = MongoClient(MONGO_URL)
-db = client["task_manager"]
+db = client[MONGO_DB]
 users_collection = db["users"]
 tasks_collection = db["tasks"]
